dc7bf5c6a3b8d8234e71d351883ce2ca65dc852a
Andreas Vilinski <vilinski@yahoo.de>
2017-07-13 08:52:39 +0200

    remove boostrapper exe

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -eu
 
 cd "$(dirname "$0")"
 
-PAKET_BOOTSTRAPPER_EXE=.paket/paket.bootstrapper.exe
 PAKET_EXE=.paket/paket.exe
 FAKE_EXE=packages/build/FAKE/tools/FAKE.exe
 
@@ -43,17 +42,17 @@ yesno() {
 }
 
 set +e
-run $PAKET_BOOTSTRAPPER_EXE
-bootstrapper_exitcode=$?
+run $PAKET_EXE restore
+paket_exitcode=$?
 set -e
 
 if [ "$OS" != "Windows_NT" ] &&
-       [ $bootstrapper_exitcode -ne 0 ] &&
+       [ $paket_exitcode -ne 0 ] &&
        [ $(certmgr -list -c Trust | grep X.509 | wc -l) -le 1 ] &&
        [ $(certmgr -list -c -m Trust | grep X.509 | wc -l) -le 1 ]
 then
   echo "Your Mono installation has no trusted SSL root certificates set up."
-  echo "This may result in the Paket bootstrapper failing to download Paket"
+  echo "This may result in the Paket failing to download Paket"
   echo "because Github's SSL certificate can't be verified. One way to fix"
   echo "this issue would be to download the list of SSL root certificates"
   echo "from the Mozilla project by running the following command:"
@@ -69,12 +68,10 @@ then
   else
     echo "Attempting to continue without running mozroots. This might fail."
   fi
-  # Re-run bootstrapper whether or not the user ran mozroots, because maybe
+  # Re-run paket whether or not the user ran mozroots, because maybe
   # they fixed the problem in a separate terminal window.
-  run $PAKET_BOOTSTRAPPER_EXE
+  run $PAKET_EXE restore
 fi
-
-run $PAKET_EXE restore
 
 [ ! -e build.fsx ] && run $PAKET_EXE update
 [ ! -e build.fsx ] && run $FAKE_EXE init.fsx
